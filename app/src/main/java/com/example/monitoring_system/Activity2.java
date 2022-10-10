54f070338da666068a3ae59d97453ff64180dc0d
breru <brusteb@siue.edu>
2022-10-10 18:13:23 -0500

    updated new ip Button

--- app/src/main/java/com/example/monitoring_system/Activity2.java
+++ app/src/main/java/com/example/monitoring_system/Activity2.java
@@ -3,14 +3,38 @@ package com.example.monitoring_system;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Activity2 extends AppCompatActivity {
+
+    int ipAddress;
+    int portNum;
+
+    EditText input1;
+    EditText input2;
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        input1 = (EditText) findViewById(R.id.input1);
+        input2 = (EditText) findViewById(R.id.input2);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               //ipAddress = input1.getText().toString();
+
+
+            }
+        });
+
 
     }
 }
